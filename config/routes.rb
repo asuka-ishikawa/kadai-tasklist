5023791ece797a360bf0bb5a09ab114ffa55815b
techtech <techtechtechtech.a>
2017-09-04 04:09:34 +0000

    top page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,22 @@
 Rails.application.routes.draw do
+  get 'toppages/index'
+
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root to: 'tasks#index'
+  root to: 'tasks#index'     #root to: 'toppages#index'
   
-  resources :tasks
-
+  get 'login', to: 'settions#new'
+  post 'login', to: 'settions#create'
+  delete 'logout', to: 'settions#destroy'
+  
+  get 'signup', to: 'users#new'
+  resources :tasks, only: [:index, :show, :new, :create]
+  
+  root to: 'toppages#index'
 end
